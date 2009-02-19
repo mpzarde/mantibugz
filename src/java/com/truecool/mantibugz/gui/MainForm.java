ffbd97f867c99ba5027c4bca98851da2a7e791d1
Martin Zardecki <mpzarde@truecool.com>
2009-02-19 00:31:57 +0000

    Initial import of Main Form.
    BugzID: 0098

---
+++ src/java/com/truecool/mantibugz/gui/MainForm.java
@@ -0,0 +1,11 @@
+package com.truecool.mantibugz.gui;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: mpzarde
+ * Date: Feb 18, 2009
+ * Time: 4:59:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MainForm {
+}
